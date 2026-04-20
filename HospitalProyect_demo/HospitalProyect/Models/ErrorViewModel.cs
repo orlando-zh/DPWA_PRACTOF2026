f8aaa37ff6a407aca8b342fa545d5e2d6d81108b
Orlando Zúniga <139894785+orlando-zh@users.noreply.github.com>
2026-04-20 17:59:39 -0600

    practica 6 y 7

--- HospitalProyect_demo/HospitalProyect/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HospitalProyect.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
